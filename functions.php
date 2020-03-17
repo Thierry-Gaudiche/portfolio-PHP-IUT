c51853478d7b48deba30a29e5962578f4d0b6b84
Thierry Gaudiche <thierry.gaudiche@mmibordeaux.com>
2020-03-17 15:08:51 +0100

    forearch projet

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
 require('connect.php');
-?>
+
+function getProjects(){
+    require('connect.php');
+    $query_project = "SELECT * FROM projets";
+    $query = $bdPdo->prepare($query_project);
+    $query->execute();
+    $project = $query->fetchALL(PDO::FETCH_OBJ);
+
+return $project;
+} ?>
