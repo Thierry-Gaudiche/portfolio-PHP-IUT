cf4013c4dfd4402d2abe16ecddac00f625f3b97b
Defless <simon.deflesschouwer@mmibordeaux.com>
2020-03-17 14:13:34 +0100

    kinda mvc setup

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('display_errors','on');
+ini_set('display_startup_errors','on');
+error_reporting(E_ALL);
+
+// Connexion et choix de la base de données
+$server = 'mysql:host=localhost;dbname=thierry_simon';
+$user = 'root';
+$password = 'root';
+
+
+try {
+	$bdPdo = new PDO($server, $user, $password);
+}
+catch (PDOException $e){
+	echo 'Problème de connexion : ' . $e->getMessage();
+
+}
+
+$utf8 = 'SET NAMES UTF8';
+$bdPdo->query($utf8);
+
+?>
