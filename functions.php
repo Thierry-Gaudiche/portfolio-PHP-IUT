89e11141f073cc2e83b3c1721784bfabef45eb86
Defless <simon.deflesschouwer@mmibordeaux.com>
2020-03-17 15:10:15 +0100

    create_project

--- functions.php
+++ functions.php
@@ -1,3 +1,50 @@
 <?php
 require('connect.php');
+
+// ====================================================================================================
+//                                       Redirections to functions
+// ====================================================================================================
+
+//------------- Articles -------------
+
+if(isset($_POST['create_project']))
+{
+    createProject();
+    header('Location:index.php');
+}
+
+if(isset($_POST['update_project']))
+{
+    updateProject();
+    header('Location:index.php');
+}
+
+if(isset($_GET['id_project_del']))
+{
+    deleteProject();
+    header('Location:index.php');
+}
+
+
+// ====================================================================================================
+//                                               Projects
+// ====================================================================================================
+
+function createProject(){
+
+    require('connect.php');
+
+    $query_create = $bdPdo->prepare('INSERT INTO projects(project_title, project_description, project_image, project_date, project_category) VALUES(:title, :description, :image, :date_project, :category)');
+
+    $query_create->execute(array(
+        ':titre' => $_POST['title'],
+        ':description' => $_POST['description'],
+        ':image' => $_POST['image'],
+        ':category' => $_POST['category'],
+        ':date' => $_POST['date'],
+    ));
+
+    $query_create->closeCursor();
+
+}
 ?>
--- new_project.php
+++ new_project.php
@@ -1,3 +1,40 @@
 <?php
 	require_once('functions.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	</head>
+	<body>
+		<form action="functions.php" method="POST">
+		  <div class="form-group">
+		    <label for="title">Titre du projet</label>
+		    <input class="form-control" name="title" placeholder="Titre">
+		  </div>
+			<div class="form-group">
+		    <label for="description">Description</label>
+		    <textarea class="form-control" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
+		  </div>
+			<div class="form-group">
+		    <label for="image">Nom du fichier image (avec l'extension)</label>
+		    <input class="form-control" name="image" placeholder="projet1.jpg">
+		  </div>
+		  <div class="form-group">
+		    <label for="category">Catégorie</label>
+		    <select name="category" class="form-control" id="exampleFormControlSelect1">
+		      <option>Web Design</option>
+		      <option>Developpement Web</option>
+		      <option>Branding</option>
+		    </select>
+		  </div>
+			<div class="form-group">
+		    <input type="submit" class="btn btn-primary" value="Créer le projet" name="create_project">
+		  </div>
+		</form>
+		<?php include('scripts.php') ?>
+	</body>
+</html>
