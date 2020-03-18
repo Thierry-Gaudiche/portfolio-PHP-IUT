70c9992f0a66980c817e83d92cf39606aa0b6543
Defless <simon.deflesschouwer@mmibordeaux.com>
2020-03-18 12:06:49 +0100

    db:migration

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 17, 2020 at 03:56 PM
+-- Generation Time: Mar 18, 2020 at 11:01 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -24,6 +24,16 @@ CREATE TABLE `categories` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(2, 'Web-design'),
+(3, 'Wev-development'),
+(4, 'Branding'),
+(5, 'Vidéo');
 
 -- --------------------------------------------------------
 
@@ -97,7 +107,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `projets`
