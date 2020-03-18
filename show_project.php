5273695106cef96ae485df95430dd5f0438481f1
Thierry Gaudiche <thierry.gaudiche@mmibordeaux.com>
2020-03-18 16:49:35 +0100

    cloudinary uploader

--- show_project.php
+++ show_project.php
@@ -80,15 +80,16 @@
        </div>
     </footer>
 
-    <!-- Javascripts -->
-    <script src="../js/jquery-2.1.4.min.js"></script>
-    <script src="../cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
-    <script src="../js/typed.js"></script>
-    <script src="../js/jquery.hover3d.js"></script>
-    <script src="../twitter-api/tweetie.js"></script>
-    <script src="../js/owl.carousel.min.js"></script>
-    <script src="../js/jquery.magnific-popup.min.js"></script>
-    <script src="../js/main.js"></script>
+		<!-- Javascripts -->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+    <script src="js/typed.js"></script>
+    <script src="js/particles.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/jquery.hover3d.js"></script>
+    <script src="twitter-api/tweetie.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
 
 </body>
 </html>
